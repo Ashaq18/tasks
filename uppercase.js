8fccd1f2c26a161822a4578dd5bdef5d5890962c
Ashaq18 <159539385+Ashaq18@users.noreply.github.com>
2024-10-21 13:11:29 +0800

    Add files via upload

---
+++ uppercase.js
@@ -0,0 +1,9 @@
+function convertToUppercase(str) {
+    return str.toUpperCase(); // Convert the string to uppercase
+}
+
+// Example Usage
+const originalString = "hello world";
+const uppercasedString = convertToUppercase(originalString);
+
+console.log(uppercasedString); // Output: "HELLO WORLD"
