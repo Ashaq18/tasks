3f79c9aa4d930168c69ca8b5a0cb65f58b00f6e4
Ashaq18 <159539385+Ashaq18@users.noreply.github.com>
2024-10-21 13:14:15 +0800

    Add files via upload

---
+++ substring.js
@@ -0,0 +1,9 @@
+const str='hello world'
+const substring="worldsss"
+if (str.includes(substring)){
+    console.log('it includes substring')
+}else{
+    console.log('it doesnt include substring')
+}
+
+
