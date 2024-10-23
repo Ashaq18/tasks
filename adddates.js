a6497b07892600ad4cbb714da21429a84c8b3c79
Ashaq18 <159539385+Ashaq18@users.noreply.github.com>
2024-10-23 16:35:48 +0800

    Add files via upload

---
+++ adddates.js
@@ -0,0 +1,6 @@
+const currentDate = new Date(); // Get the current date
+
+// Add 7 days to the current date
+currentDate.setDate(currentDate.getDate() + 7);
+
+console.log(currentDate); // This will show the date 7 days from today
